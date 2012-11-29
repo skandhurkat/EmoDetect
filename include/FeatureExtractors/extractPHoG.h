169c1f88cecac7e1bf9017800a28b42e87b7ef2f
Skand Hurkat <sh953@cornell.edu>
2012-11-29 14:39:20 -0500

    Added GNU GPL v3 to all files

--- include/FeatureExtractors/extractPHoG.h
+++ include/FeatureExtractors/extractPHoG.h
@@ -1,3 +1,17 @@
+//    This file is part of EmoDetect.
+//
+//    EmoDetect is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    EmoDetect is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with EmoDetect. If not, see <http://www.gnu.org/licenses/>.
 #ifndef PHOG_3_H_INCLUDED
 #define PHOG_3_H_INCLUDED
 
